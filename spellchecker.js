af4f2e2224aafddfe863780f3587a28685e902fd
Ian Kronquist <iankronquist@teleport.com>
2014-12-15 20:53:44 -0800

    Add skeleton code for the spellchecker
    This is to define the API so I can write tests against it. Note that I
    chose to pass an explicit alphabet to inserts, replaces, known_edits,
    and known. This has two goals. The first is that it is a more of a
    functional style, as is fitting for modern javascript. The second is
    that it's easier to test.

---
+++ spellchecker.js
@@ -0,0 +1,39 @@
+function edits(word, alphabet) {
+
+}
+
+function train(dictionary_name) {
+
+}
+
+function known_edits(word) {
+
+}
+
+function known(words) {
+
+}
+
+function _splits(word) {
+
+}
+
+function _deletes(word) {
+
+}
+
+function _transposes(word) {
+
+}
+
+function _replaces(word, alphabet) {
+
+}
+
+function _inserts(word, alphabet) {
+
+}
+
+function correct(words) {
+
+}
